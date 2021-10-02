2aab5c450422acf46b140278c25302ce8530e7a0
benhuang0857 <benhuang0857@gmail.com>
2021-10-03 03:39:28 +0800

    20211003 predict hno3

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:tank')->hourly();
+        $schedule->command('update:tank')->cron('*/2 * * * * *');
     }
 
     /**
